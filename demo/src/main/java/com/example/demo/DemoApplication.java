1ca3b01fcd46f1fa476d441649104b8243e155cf
Piotr Pawlak <piotrpawlak123@vp.pl>
2020-11-15 11:52:09 +0100

    correct sleep

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,53 +8,56 @@ import java.sql.*;
 @SpringBootApplication
 public class DemoApplication {
 
-	static final String DB_URL = "jdbc:mysql://Full2020-86383:3306/demo?useSSL=false&serverTimezone=UTC";
+    static final String DB_URL = "jdbc:mysql://Full2020-86383:3306/demo?useSSL=false&serverTimezone=UTC";
 //	static final String DB_URL = "jdbc:mysql://localhost:3306/demo?useSSL=false&serverTimezone=UTC";
 //	static final String DB_URL = "jdbc:mysql://localhost:3306/?serverTimezone=UTC";
 
-	static final String USER = "ppawlak";
-	static final String PASS = "ppawlak";
-	static final String DB_NAME = "demo";
+    static final String USER = "ppawlak";
+    static final String PASS = "ppawlak";
+    static final String DB_NAME = "demo";
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-		Thread.sleep(40000);
+        Thread.sleep(10000);
 
-		Connection connection = null;
-		Statement statement = null;
-		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Connection connection = null;
 
-			createDataBaseIfNotExists(connection,DB_NAME);
-			statement = connection.createStatement();
+        try {
+            for (int i = 0; i < 20; i++) {
 
-		}catch(SQLException se){
-			se.printStackTrace();
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			try{
-				if(statement!=null)
-					statement.close();
-			}catch(SQLException se2){
-			}
-			try{
-				if(connection!=null)
-					connection.close();
-			}catch(SQLException se){
-				se.printStackTrace();
-			}
-		}
-		SpringApplication.run(DemoApplication.class, args);
-	}
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    connection = DriverManager.getConnection(DB_URL, USER, PASS);
 
-	public static void createDataBaseIfNotExists(Connection connection, String dbName) throws SQLException {
-		Statement statement = connection.createStatement();
-		statement.execute("CREATE DATABASE IF NOT EXISTS " + dbName +";");
-		statement.close();
-		Statement statement2 = connection.createStatement();
-		statement2.execute("CREATE TABLE IF NOT EXISTS demo.users (`id` int(11) NOT NULL AUTO_INCREMENT, `login` varchar(60) DEFAULT NULL, `password` varchar(60) DEFAULT NULL, PRIMARY KEY (`id`))");
-		statement2.close();
-	}
+                    createDataBaseIfNotExists(connection, DB_NAME);
+
+                    break;
+
+                } catch (Exception e) {
+                    if (i == 19) {
+                        e.printStackTrace();
+                    }
+                }
+                Thread.sleep(5000);
+            }
+
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    public static void createDataBaseIfNotExists(Connection connection, String dbName) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE DATABASE IF NOT EXISTS " + dbName + ";");
+        statement.close();
+        Statement statement2 = connection.createStatement();
+        statement2.execute("CREATE TABLE IF NOT EXISTS demo.users (`id` int(11) NOT NULL AUTO_INCREMENT, `login` varchar(60) DEFAULT NULL, `password` varchar(60) DEFAULT NULL, PRIMARY KEY (`id`))");
+        statement2.close();
+    }
 }
